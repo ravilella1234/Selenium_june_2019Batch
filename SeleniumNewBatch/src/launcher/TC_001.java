0035b38ec9a42c64f29e078bbb6dd171e950db9d
sai <ravilella.1981@gmail.com>
2019-05-30 09:19:04 +0530

    Browser Commands added

--- SeleniumNewBatch/src/launcher/TC_001.java
+++ SeleniumNewBatch/src/launcher/TC_001.java
@@ -1,5 +1,7 @@
 package launcher;
 
+import org.openqa.selenium.By;
+
 public class TC_001 extends BaseTest
 {
 
@@ -9,7 +11,39 @@ public class TC_001 extends BaseTest
 		
 		launch("chromebrowser");
 		
-		navigateUrl("bestbuyurl");
+		navigateUrl("hdfcurl");
+		
+		driver.manage().window().maximize();
+		
+		/*String title = driver.getTitle();
+		System.out.println(title);
+		
+		
+		String url = driver.getCurrentUrl();
+		System.out.println(url);
+		
+		driver.manage().deleteAllCookies();
+		
+		driver.navigate().back();
+		
+		Thread.sleep(3000);
+		
+		driver.navigate().forward();
+		
+		Thread.sleep(3000);
+		
+		driver.navigate().refresh();*/
+		
+		Thread.sleep(3000);
+		
+		driver.findElement(By.xpath("//img[@class='popupCloseButton']")).click();
+		
+		driver.findElement(By.id("loginsubmit")).click();
+		
+		driver.close();
+		
+		//driver.quit();
+
 
 	}
 
